81231172013807331fb9f4da4ae189308a8d0e90
indrajeetsuryawanshi <indrajeetsuryawanshi2592@gmail.com>
2024-05-06 17:22:57 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', bookRoutes);
+app.get( (req, res) => {
+  res.send('Hello from the API!');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
